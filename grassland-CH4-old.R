1f1a513bc6da01dda2811451fea77cc9c659c302
otto <ottobriner@gmail.com>
2022-05-06 15:46:28 -0500

    R markdown, starting revisions

--- grassland-CH4-old.R
+++ grassland-CH4-old.R
@@ -11,8 +11,7 @@ read_site <- function(fp) {
 append_annual <- function(df, annual) {
   df %>%
   group_by(year = floor_date(as_date(date), 'year')) %>% 
-  summarize(FCH4_an = sum(FCH4_F, na.rm = TRUE),
-            NEE_an = sum(NEE, na.rm = TRUE)) %>% 
+  summarize(FCH4_an = sum(FCH4_F, na.rm = TRUE)) %>% 
   filter(FCH4_an != 0) %>% 
   mutate(site = df$site[1]) %>% 
   full_join(annual)
@@ -61,7 +60,7 @@ df_NLhor = read_site(fp) %>%
   mutate(site = 'NL-Hor')
 
 fp = 'data/FLUXNET-CH4/FLX_SE-Deg_FLUXNET-CH4_2014-2018_1-1/FLX_SE-Deg_FLUXNET-CH4_HH_2014-2018_1-1.csv'
-df_USdeg = read_site(fp) %>% 
+df_USsne = read_site(fp) %>% 
   mutate(site = 'SE-Deg')
 
 fp = 'data/FLUXNET-CH4/FLX_US-NGC_FLUXNET-CH4_2017-2018_1-1/FLX_US-NGC_FLUXNET-CH4_HH_2017-2018_1-1.csv'
@@ -79,8 +78,7 @@ df_USsne = read_site(fp) %>%
 # aggregate annual fluxes
 annual <- df_ATneu %>%
   group_by(year = floor_date(as_date(date), 'year')) %>% 
-  summarize(FCH4_an = sum(FCH4_F, na.rm = TRUE),
-            NEE_an = sum(NEE, na.rm = TRUE)) %>% 
+  summarize(FCH4_an = sum(FCH4_F, na.rm = TRUE)) %>% 
   filter(FCH4_an != 0) %>% 
   mutate(site = df_ATneu$site[1])
 
@@ -93,25 +91,9 @@ annual <- append_annual(df_USngc, annual)
 annual <- append_annual(df_USsnd, annual)
 annual <- append_annual(df_USsne, annual)
 
-# compute annual radiative balance
-# NEE (umol m-2 s-1) * 1 + FCH4 (nmol m-2 s-1) * 34
-annual <- annual %>% 
-  mutate(rad = NEE_an + FCH4_an * 34 * 10^-3)
-annual
-
-# plot annual ch4 fluxes
 ggplot(annual, aes(x = year)) + 
   geom_point(aes(y=FCH4_an, color=site)) +
   theme_minimal()
-
-# plot annual NEE
-ggplot(annual, aes(x = year)) + 
-  geom_point(aes(y=NEE_an, color=site)) +
-  theme_minimal()
-
-# plot radiative balance (NEE*1 + FCH4*34)
-
-
 
 # aggregate monthly fluxes
 monthly <- df_ATneu %>%
@@ -180,3 +162,89 @@ p2 <- ggplot(df_USsne, aes(date, y=WTD)) +
   geom_line()
 
 p1 / p2
+
+# junk plots, old
+
+ggplot(df_CHcha, aes(x=date)) + 
+  geom_point(aes(y=FCH4))
+
+ggplot(df_CNhgu, aes(x=date)) + 
+  geom_point(aes(y=FCH4))
+
+ggplot(df_ATneu, aes(x=date)) + 
+  geom_point(aes(y=FCH4))
+
+ggplot(df_CHcha, aes(x=TA, y=P, color=FCH4)) + geom_point()
+
+df_CHcha %>% 
+  filter(!is.na(FCH4)) %>% 
+  ggplot(aes(TA, P, color=FCH4)) + geom_point() +
+  theme_minimal()
+# ggsave('Documents/579/grassland-CH4/TvsPvsFCH4_CH-Cha.png')
+
+df_CHcha %>% 
+  filter(!is.na(FCH4_F)) %>%
+  ggplot(aes(TA, P, color=FCH4_F)) + 
+  geom_point() +
+  theme_minimal()
+
+df_CHcha %>% 
+  filter(!is.na(FCH4_F)) %>%
+  ggplot(aes(date, FCH4_F)) + 
+  geom_point() +
+  theme_minimal()
+
+df_ATneu %>% 
+  filter(is.na(FCH4_F))
+
+df_CHcha %>%  
+  filter(is.na(df_CHcha['FCH4_F']))
+
+annual <- df_CHcha %>% 
+  group_by(floor_date(as_date(date), 'year')) %>% 
+  summarize(FCH4_an = sum(FCH4_F, na.rm = TRUE)) %>% 
+  filter(FCH4_an != 0)
+
+df_CHcha %>% 
+  group_by(year(date)) %>% 
+  summarize(FCH4_Fansum=sum(FCH4_F))
+
+df_CNhgu %>% 
+  group_by(year(date)) %>% 
+  summarize(FCH4_Fansum=sum(FCH4_F))
+
+df_ATneu %>% 
+  group_by(year(date)) %>% 
+  summarize(FCH4_Fansum=sum(FCH4_F))
+
+# # An example of unzipped BASE files from the above zipped file
+# f_CHCha <- system.file("extdata", 'FLX_CH-Cha_FLUXNET-CH4_HH_2012-2016_1-1.csv', package = "amerifluxr")
+# 
+# # read the BASE from a csv file, with additional parsed time-keeping columns
+# base2 <- amf_read_base(
+#   file = f_CHCha,
+#   unzip = FALSE,
+#   parse_timestamp = TRUE
+# )
+# 
+# df_CHcha <- read_csv(fp, na='-9999') %>% 
+#   mutate(date = parse_datetime(as.character(TIMESTAMP_START), '%Y%m%d%H%M'),
+#          site = 'CH-Cha')
+# df_CNhgu <- read_csv(fp, na='-9999') %>% 
+#   mutate(date = parse_datetime(as.character(TIMESTAMP_START), '%Y%m%d%H%M'),
+#          site = 'CN-Hgu')
+# df_ATneu <- read_csv(fp, na='-9999') %>% 
+#   mutate(date = parse_datetime(as.character(TIMESTAMP_START), '%Y%m%d%H%M'))
+# annual <- df_CNhgu %>%
+#   group_by(year = floor_date(as_date(date), 'year')) %>% 
+#   summarize(FCH4_an = sum(FCH4_F, na.rm = TRUE)) %>% 
+#   filter(FCH4_an != 0) %>% 
+#   mutate(site = df_CNhgu$site[1]) %>% 
+#   full_join(annual)
+# 
+# annual <- df_ATneu %>%
+#   group_by(year = floor_date(as_date(date), 'year')) %>% 
+#   summarize(FCH4_an = sum(FCH4_F, na.rm = TRUE)) %>% 
+#   filter(FCH4_an != 0) %>% 
+#   mutate(site = df_ATneu$site[1]) %>% 
+#   full_join(annual)
